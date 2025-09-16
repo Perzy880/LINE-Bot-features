a49052d2af5ac25c2090647ceda647b165558cfd
Perzy880 <dachathonk@hotmail.com>
2025-09-16 13:57:01 +0700

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,9 +1,30 @@
 const axios = require('axios');
 
-async function getWeather() {
-  const response = await axios.get('https://apis.thaiskyward.com/v1/weather/13.7563,100.5018/?api_key=YOUR_API_KEY');
-  const weather = response.data.nowcasting;
-  return `พยากรณ์อากาศวันนี้:\nอุณหภูมิ: ${weather.temperature}°C\nความชื้น: ${weather.humidity}%\nสภาพอากาศ: ${weather.description}`;
+async function getWeather(lat = 13.7563, lon = 100.5018) {
+  try {
+    const response = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+      params: {
+        lat,
+        lon,
+        appid: 'd3c39e210bf80691ddc15c9f4c12d36c',
+        units: 'metric',
+        lang: 'th'
+      }
+    });
+
+    const data = response.data;
+    const temp = data.main.temp;
+    const humidity = data.main.humidity;
+    const description = data.weather[0].description;
+    const location = data.name;
+
+    return `พยากรณ์อากาศที่ ${location}:
+อุณหภูมิ: ${temp}°C
+ความชื้น: ${humidity}%
+สภาพอากาศ: ${description}`;
+  } catch (error) {
+    return 'ไม่สามารถดึงข้อมูลพยากรณ์อากาศได้ในขณะนี้';
+  }
 }
 
 module.exports = { getWeather };
