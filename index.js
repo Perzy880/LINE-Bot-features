c9725e9b9ebde0e6e81ccc39ad4f66c61f0736ad
Perzy880 <dachathonk@hotmail.com>
2025-09-16 14:59:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,20 @@ const handler = require('./features/handler');
 const app = express();
 app.use(bodyParser.json());
 
-const ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN;
+//const ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN;
 
-app.post('/webhook', async (req, res) => {
-  const events = req.body.events;
-  for (const event of events) {
-    if (event.type === 'message' && event.message.type === 'text') {
-      await handler.handleMessage(event, ACCESS_TOKEN);
-    }
-  }
-  res.sendStatus(200);
-});
+//app.post('/webhook', async (req, res) => {
+ // const events = req.body.events;
+  //for (const event of events) {
+  //  if (event.type === 'message' && event.message.type === 'text') {
+  //    await handler.handleMessage(event, ACCESS_TOKEN);
+  //  }
+ // }
+ // res.sendStatus(200);
+//});
+
+// เรียก handler โดยตรง
+app.post('/webhook', handler);
 
 app.get('/', (req, res) => {
   res.send('LINE Bot is running!');
