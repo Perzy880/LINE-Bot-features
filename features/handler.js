e1a87cf6c670955211d7b57d81750e598d9a08a1
Perzy880 <dachathonk@hotmail.com>
2025-09-18 12:26:35 +0700

    Add files via upload

--- features/handler.js
+++ features/handler.js
@@ -14,6 +14,116 @@ exports.handler = async (req, res) => {
   const event = req.body.events[0];
   const message = event.message.text;
 
+  if (message === 'ราคาทอง') {
+    try {
+      const response = await axios.get('https://api.chnwt.dev/thai-gold-api/latest');
+      const data = response.data.response;
+      const date = data.date;
+      const time = data.update_time;
+      const goldBar = data.price.gold_bar;
+      const goldJewelry = data.price.gold;
+
+      await client.replyMessage(event.replyToken, {
+        type: 'flex',
+        altText: 'ราคาทองคำ',
+        contents: {
+          type: 'bubble',
+          body: {
+            type: 'box',
+            layout: 'vertical',
+            spacing: 'md',
+            contents: [
+              {
+                type: 'text',
+                text: 'ราคาทองคำ',
+                weight: 'bold',
+                size: 'xl',
+                align: 'center'
+              },
+              {
+                type: 'text',
+                text: `วันที่: ${date}`,
+                size: 'sm',
+                color: '#888888',
+                align: 'center'
+              },
+              {
+                type: 'text',
+                text: `เวลาอัปเดต: ${time}`,
+                size: 'sm',
+                color: '#888888',
+                align: 'center'
+              },
+              {
+                type: 'separator',
+                margin: 'md'
+              },
+              {
+                type: 'text',
+                text: 'ทองแท่ง',
+                weight: 'bold',
+                size: 'md',
+                margin: 'md'
+              },
+              {
+                type: 'box',
+                layout: 'horizontal',
+                contents: [
+                  {
+                    type: 'text',
+                    text: `ขายออก: ${goldBar.sell} บาท`,
+                    size: 'sm',
+                    flex: 1
+                  },
+                  {
+                    type: 'text',
+                    text: `รับซื้อ: ${goldBar.buy} บาท`,
+                    size: 'sm',
+                    flex: 1,
+                    align: 'end'
+                  }
+                ]
+              },
+              {
+                type: 'text',
+                text: 'ทองรูปพรรณ',
+                weight: 'bold',
+                size: 'md',
+                margin: 'md'
+              },
+              {
+                type: 'box',
+                layout: 'horizontal',
+                contents: [
+                  {
+                    type: 'text',
+                    text: `ขายออก: ${goldJewelry.sell} บาท`,
+                    size: 'sm',
+                    flex: 1
+                  },
+                  {
+                    type: 'text',
+                    text: `รับซื้อ: ${goldJewelry.buy} บาท`,
+                    size: 'sm',
+                    flex: 1,
+                    align: 'end'
+                  }
+                ]
+              }
+            ]
+          }
+        }
+      });
+      return res.status(200).end();
+    } catch (error) {
+      await client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: 'ขออภัย ไม่สามารถดึงข้อมูลราคาทองได้ในขณะนี้'
+      });
+      return res.status(200).end();
+    }
+  }
+
   if (message === 'เมนู') {
     await client.replyMessage(event.replyToken, {
       type: 'flex',
@@ -46,72 +156,6 @@ exports.handler = async (req, res) => {
       }
     });
     return res.status(200).end();
-  }
-
-  if (message === 'ราคาทอง') {
-    try {
-      const response = await axios.get('https://api.chnwt.dev/thai-gold-api/latest');
-      const data = response.data.response;
-      const gold = data.price.gold;
-      const goldBar = data.price.gold_bar;
-      const date = data.date;
-      const time = data.update_time;
-
-      await client.replyMessage(event.replyToken, {
-        type: 'flex',
-        altText: 'ราคาทองวันนี้',
-        contents: {
-          type: 'bubble',
-          body: {
-            type: 'box',
-            layout: 'vertical',
-            contents: [
-              {
-                type: 'text',
-                text: 'ราคาทองวันนี้',
-                weight: 'bold',
-                size: 'md',
-                margin: 'md'
-              },
-              {
-                type: 'text',
-                text: `ทองรูปพรรณ ขายออก: ${gold.sell} บาท`,
-                margin: 'sm'
-              },
-              {
-                type: 'text',
-                text: `ทองรูปพรรณ รับซื้อ: ${gold.buy} บาท`,
-                margin: 'sm'
-              },
-              {
-                type: 'text',
-                text: `ทองคำแท่ง ขายออก: ${goldBar.sell} บาท`,
-                margin: 'sm'
-              },
-              {
-                type: 'text',
-                text: `ทองคำแท่ง รับซื้อ: ${goldBar.buy} บาท`,
-                margin: 'sm'
-              },
-              {
-                type: 'text',
-                text: `ปรับล่าสุด: ${date} ${time}`,
-                margin: 'sm',
-                size: 'xs',
-                color: '#888888'
-              }
-            ]
-          }
-        }
-      });
-      return res.status(200).end();
-    } catch (error) {
-      await client.replyMessage(event.replyToken, {
-        type: 'text',
-        text: 'ขออภัย ไม่สามารถดึงข้อมูลราคาทองได้ในขณะนี้'
-      });
-      return res.status(200).end();
-    }
   }
 
   await client.replyMessage(event.replyToken, {
